c1ee448cc5355cf9df5110c60672dae59c082441
vlady1 <vlady_temp@abv.bg>
2015-06-21 16:03:49 +0300

    Create zadacha4.php

---
+++ zadacha4.php
@@ -0,0 +1,9 @@
+<?php
+	$BGN = 2.50;
+	$dolar = 1.95;
+	echo "Fiksiran kursa na dolara : $dolar <br/>";
+	echo "Zadacha: Da preobrazuvame 2.50lv, spored kursa na dolara za denia! <br/>";
+	echo "Otgovor:";
+	echo number_format($BGN / $dolar,2);
+
+?>
