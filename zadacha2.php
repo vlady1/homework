0683cb08694e11151d645375eeb24dd670a0d32e
vlady1 <vlady_temp@abv.bg>
2015-06-21 15:42:06 +0300

    Create zadacha2.php

---
+++ zadacha2.php
@@ -0,0 +1,13 @@
+<?php
+	$Var=true;
+	$Var1=1.56;
+	$Var2=10;
+	$Var3="Name";
+	var_dump($Var);
+	echo "<br/>";
+	var_dump($Var1);
+	echo "<br/>";
+	var_dump($Var2);
+	echo "<br/>";
+	var_dump($Var3);
+?>
