849ed1a21c8b9cb18802fed61fc70e4d0763ca98
vlady1 <vlady_temp@abv.bg>
2015-06-21 15:46:33 +0300

    Create zadacha3.php

---
+++ zadacha3.php
@@ -0,0 +1,13 @@
+<?php
+	$a = 6;
+	$b = 10;
+	echo "<b>Da se nameri litseto i perimeryta na pravoygylnika, sys strani a = 6 i b = 10</b><br/>";
+	echo "<br/>";
+	echo "Reshenie:<br/>";
+	echo "<br/>";
+	echo "Litseto na pravoygylnika e S =". $a * $b; // Lice na pravoygylnik
+	echo "<br/>";
+	echo "Perimetyra na pravoygylnika e P =";
+	echo  ($a + $b)*2; //Perimetyr na triygylnika
+
+?>
