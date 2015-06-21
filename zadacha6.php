31021c0421ab05a7935046bf65b2a83f7e42d381
vlady1 <vlady_temp@abv.bg>
2015-06-21 16:09:05 +0300

    Create zadacha6.php

---
+++ zadacha6.php
@@ -0,0 +1,6 @@
+<?php
+$movies = array("Braveheart" => "Mel Gibson", "Gladiator" => "Russell Crowe", "Ocean's Eleven" => "Brad Pitt", "FastandFurious" => "Vin Diesel", "Training Day" => "Denzel Washington");
+echo "Main actor in the movie \"Braveheart\" is : $movies[Braveheart]";
+echo "<br/>";
+echo "Main actor in the movie \"Fast and Furious\" is : $movies[FastandFurious]";
+?>
